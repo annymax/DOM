25f3531c4dd9b37a8af878ace6a777ea2220233f
annymax <107364625+annymax@users.noreply.github.com>
2022-09-23 21:17:48 +0300

    added dom task

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const basketPlayers = [
-  { id:"0",
+  {
+    id: "0",
     name: "Michael Jordan",
     photo: "./images/jordan.jpg",
     facts: `Michael Jordan, in full Michael Jeffrey Jordan,
@@ -9,7 +10,8 @@ const basketPlayers = [
   He led the Chicago Bulls to six National Basketball Association (NBA) 
   championships (1991–93, 1996–98).`,
   },
-  { id:"1",
+  {
+    id: "1",
     name: "Lebron James",
     photo: "./images/lebron.jpg",
     facts: `LeBron James became an immediate star after skipping 
@@ -17,7 +19,8 @@ const basketPlayers = [
   titles in 2012 and 2013 and won another championship 
   with Cleveland in 2016, before joining the Los Angeles Lakers in 2018.`,
   },
-  { id:"2",
+  {
+    id: "2",
     name: "Luka Doncic",
     photo: "./images/doncic.jpg",
     facts: `Luka Doncic is a Slovenian professional basketball player for the 
@@ -25,7 +28,8 @@ const basketPlayers = [
   He has broken several NBA and EuroLeague champion history records and has been 
   named NBA All-Star, All-NBA First Team, and NBA Rookie of the Year.`,
   },
-  { id:"3",
+  {
+    id: "3",
     name: "James Harden",
     photo: "./images/Harden.jpg",
     facts: `James Edward Harden Jr. (born August 26, 1989) is an American professional basketball player 
@@ -33,29 +37,30 @@ const basketPlayers = [
   as well as one of the top overall players in the league.Harden is a two-time member of the United States national team,
    winning gold medals at the 2012 Summer Olympics and 2014 FIBA World Cup.`,
   },
-  { id:"4",
+  {
+    id: "4",
     name: "Russel Westbrook",
     photo: "./images/westbrook.jpg",
     facts: `Russell Westbrook III 
   (born November 12, 1988) is an American professional basketball player for the Los Angeles 
-  Lakers of the National Basketball Association (NBA). A member of the NBA 75th Anniversary Team,
-   he is a nine-time NBA All-Star and earned the NBA Most Valuable Player Award (MVP) for the 2016–17 
-   season. He is also a nine-time All-NBA Team member, 
-  led the league in scoring in 2014–15 and 2016–17, and won back-to-back NBA All-Star Game MVP awards in 2015 and 2016.
-In 2017, the year he won the league MVP award, Westbrook became one of two players in NBA history 
-   to average a triple-double for a season, along with Oscar Robertson in 1962. He also set a record 
-   for the most triple-doubles in a season, with 42. He went on to average a triple-double the 
-   following two seasons as well as lead the league in assists and become the first player to lead the 
-   league in points and assists in multiple seasons. In 2020–21, Westbrook averaged a triple-double 
+  Lakers of the National Basketball Association (NBA).  He is also a nine-time All-NBA Team member, 
+  led the league in scoring in 2014–15 and 2016–17, and won back-to-back NBA All-Star Game MVP awards
+   in 2015 and 2016.
+ In 2020–21, Westbrook averaged a triple-double 
    for the fourth time in five seasons, and he passed Robertson for the most career triple-doubles in NBA history.`,
   },
-  { id:"5",
-    name: "Kobe Bryant", photo: "./images/kobe.jpg", facts: `Kobe Bryant, in full Kobe Bean Bryant, 
+  {
+    id: "5",
+    name: "Kobe Bryant",
+    photo: "./images/kobe.jpg",
+    facts: `Kobe Bryant, in full Kobe Bean Bryant, 
   (born August 23, 1978, Philadelphia, Pennsylvania, U.S.—died January 26, 2020, Calabasas, California), 
   American professional basketball player, who helped lead the Los Angeles Lakers of the National 
   Basketball
-   Association (NBA) to five championships (2000–02 and 2009–10).` },
-  { id:"6",
+   Association (NBA) to five championships (2000–02 and 2009–10).`,
+  },
+  {
+    id: "6",
     name: "Stephen Curry",
     photo: "./images/Curry.jpeg",
     facts: `Wardell Stephen Curry II (born March 14, 1988) is an American 
@@ -67,8 +72,7 @@ In 2017, the year he won the league MVP award, Westbrook became one of two playe
   },
 ];
 
-
-const button = document.querySelector(".nav-btn");
+const button = document.querySelector(".image-ball");
 const menu = document.querySelector(".menu");
 const content = document.querySelector(".content-wrapper");
 const siteTitle = document.querySelector(".site-title");
@@ -81,26 +85,25 @@ button.addEventListener("click", function () {
 
 const bodyElement = document.body;
 
-function showPlayerElement(player){
-    const h2= document.createElement("h2");
-    const image = document.createElement("img");
-    const info = document.createElement("p");
-    content.innerHTML = "";
-    h2.className = "title";
-    h2.innerHTML = player.name;
-    image.className = "img-basket";
-   image.src = player.photo;
-    info.className = "about";
-   info.innerHTML = player.facts;
-   image.className = "img-basket";
+function showPlayerElement(player) {
+  const h2 = document.createElement("h2");
+  const image = document.createElement("img");
+  const info = document.createElement("p");
+  content.innerHTML = "";
+  h2.className = "title";
+  h2.innerHTML = player.name;
+  image.className = "image-player";
+  image.setAttribute("alt", player.name);
   image.src = player.photo;
-    content.prepend(h2, image, info);
-  };
+  info.className = "about";
+  info.innerHTML = player.facts;
+  content.prepend(h2, image, info);
+}
 
-document.querySelector(".menu-list").addEventListener("click", ({target}) => {
- if (!target.matches(".menu-list-link")) return undefined;
-    const player = basketPlayers.find((player) => {
+document.querySelector(".menu-list").addEventListener("click", ({ target }) => {
+  if (!target.matches(".menu-list-link")) return undefined;
+  const player = basketPlayers.find((player) => {
     return player.id === target.id;
   });
-   showPlayerElement(player);
+  showPlayerElement(player);
 });
